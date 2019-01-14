7247640f424d89ca04a88f8e7ec70cda60f5b1f1
Golam Mahmud Rafi <rafi.aust@live.com>
2019-01-15 02:27:30 +0900

    Code added

---
+++ resources/js/app.js
@@ -0,0 +1,31 @@
+require('./bootstrap');
+
+// Vue
+import Vue from 'vue'
+import App from './components/App'
+
+// Vuetify
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+    theme: {
+        primary: colors.indigo.base,
+        secondary: colors.blue.base,
+        accent: colors.amber.base,
+    }
+});
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+
+// Vue-Router
+import router from './router'
+
+
+// Main app
+const app = new Vue({
+    el: '#app',
+    render: h => h(App),
+    router,
+});
---
+++ resources/js/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+//
+import example_component from '../components/ExampleComponent.vue'
+import admin_component from '../components/AdminComponent.vue'
+import login_component from '../components/LoginComponent.vue'
+import r_link from '../components/RouterLink.vue'
+
+//
+Vue.component('example-component', example_component)
+Vue.component('admin-component', admin_component)
+Vue.component('r-link', r_link)
+Vue.component('login-component', login_component)
+
+
+//
+import home from '../components/HomeComponent.vue'
+import admin_user from '../components/Admin/UserComponent.vue'
+
+export default new Router({
+    mode: 'history',
+    routes: 
+    [
+        {
+            path: '/admin/login',
+            name: 'login',
+            meta: 'login'
+        },
+        { 
+            path: '/admin/home', 
+            name: 'home', 
+            component: home, 
+            meta: 
+            { 
+                name: 'ホーム', 
+                icon: 'home' ,
+                type: 'admin'
+            } 
+        },
+        { 
+            path: '/admin/user', 
+            name: 'admin_user', 
+            component: admin_user, 
+            meta: 
+            { 
+                name: '社員管理', 
+                icon: 'supervisor_account',
+                type: 'admin'
+            } 
+        },
+    ],
+})
